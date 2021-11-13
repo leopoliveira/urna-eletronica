a526c443d5611cb8307b11831d40d6121fd56c51
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2021-11-13 11:56:30 -0300

    commit

---
+++ public/js/BaseDados.js
@@ -0,0 +1,39 @@
+const prefeitos = [
+    {
+        numero: 84,
+        dados: {
+            nome: "Cowboy do Povo", 
+            foto: "/public/images/84.jpg", 
+            vice: "Cowgirl do Povo", 
+            fotoVice: "/public/images/84_2.jpg"
+        }
+    },
+
+    {
+        numero: 99,
+        dados: {
+            nome: "Maicon Taison", 
+            foto: "/public/images/99.jpg", 
+            vice: "Victorino Belfraco", 
+            fotoVice: "/public/images/99_2.jpg"
+        }
+    }
+];
+
+const vereadores = [
+    {
+        numero: 38111,
+        dados: {
+            nome: "Flash", 
+            foto: "/public/images/38111.jpg"
+        }
+    },
+
+    {
+        numero: 77222,
+        dados: {
+            nome: "Judith", 
+            foto: "/public/images/77222.jpg"
+        }
+    }
+];
